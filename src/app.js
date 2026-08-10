be8975a5f8fe3c923445115a9ccad59a97d8c142
Sakshi <sakshi.singh3746@gmail.com>
2026-08-10 23:22:29 +0530

    Db and API validations

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const {User}=require('./models/user');
 const app= express();
 
 app.use(express.json());
+
+
 app.post('/signup',async(req,res)=>{
     const entry=new User(req.body);
     
@@ -30,6 +32,37 @@ app.post('/signup',async(req,res)=>{
      }
 
 });
+
+
+//a simple GET API to get the user data based on their email from the db
+app.get('/getUser',async(req,res)=>{
+    try{
+
+    console.log(req.body);
+        
+    const email=req.query.emailId;
+    const data=await User.find({emailId:email});
+   
+    res.send(data);
+   }
+   catch(err)
+   {
+    console.log("Hitting catch block");
+    res.status(404).send("User doesn't exist with us!");
+   }
+});    
+
+app.get('/getFeed', async (req,res)=>{
+    try{
+     const users= await User.find({});
+     res.send(users);
+    }
+    catch(err)
+    {
+        res.status(400).send("Something went wrong");
+    }
+})
+
 
 
 makeConnection().then(()=>{
